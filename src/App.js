507f37b429a84bb7302f59bf5dc1c3e5e6856278
abrnsyh <abriansyah1099@gmail.com>
2022-07-13 18:51:26 +0700

    Add a nav using bootstrap and a router to it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Error from './pages/Error'
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { Navbar } from "react-bootstrap";
+
+import NavBar from "./components/NavBar/NavBar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route exact path='/' element={<Home />}/>
+        <Route path='/sign-up' element={<Signup />}/>
+        <Route path='/log-in' element={<Login />}/>
+        <Route path='*' element={<Error />}/>
+      </Routes>
+    </>
   );
 }
 
