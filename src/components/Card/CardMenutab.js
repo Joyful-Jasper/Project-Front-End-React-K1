7bee1b94ccc75d865708c6f547fcecad2ba9a9e6
SalwaNurulAisha <salwa8l.25@gmail.com>
2022-07-19 11:03:36 +0700

    adding detail books

--- src/components/Card/CardMenutab.js
+++ src/components/Card/CardMenutab.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CardMenutab.css";
 import Card from "./Card";
 import DetailBooks from "../DetailBooks-Modal/DetailBooks";
 
 const CardMenutab = (props) => {
+  const [book, setBook] = useState({});
+
   return (
     <>
       {/* Card */}
       <div className="container">
         <h4 className="title">Romance</h4>
         <div className="row">
-          {props?.data?.map((item) => (
-            <div className="col-md-3 my-2">
+          {props?.data?.map((item, index) => (
+            <div className="col-md-3 my-2" key={index}>
               <Card className="image-card" imgsrc={item?.volumeInfo?.imageLinks?.thumbnail} title={item?.volumeInfo?.title} />
-              <div className="middle">
-                <div className="text">
-                  <DetailBooks />
-                </div>
-              </div>
+              {/* Button trigger modal */}
+              <button onClick={() => setBook(item.volumeInfo)} type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" style={{ display: "flex", alignItems: "center", margin: "auto" }}>
+                Details Book
+              </button>
             </div>
           ))}
+        </div>
+        <div className="middle">
+          <div className="text">
+            <DetailBooks book={book} />
+          </div>
         </div>
       </div>
     </>
--- src/components/DetailBooks-Modal/DetailBooks.js
+++ src/components/DetailBooks-Modal/DetailBooks.js
@@ -1,17 +1,13 @@
 import React from "react";
 import "./DetailBooks.css";
 
-const DetailBooks = (props) => {
+const DetailBooks = ({ book }) => {
+  console.log(book);
   return (
     <div>
-      {/* Button trigger modal */}
-      <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        Details Book
-      </button>
-
       {/* Modal  */}
       <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog">
+        <div className="modal-dialog modal-dialog-scrollable">
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title" id="exampleModalLabel">
@@ -20,11 +16,13 @@ const DetailBooks = (props) => {
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
-              <button className="shelf-book">No. shelf : {props.shelf}</button>
-              <img src={props.cover} className="cover-book" />
-              <h3 className="title-book">{props.title}</h3>
-              <p className="release-book">{props.release}</p>
-              <h6 className="preview-book">{props.preview}</h6>
+              <button className="shelf-book">Page Count : {book.pageCount}</button>
+              <img src={book.imageLinks.thumbnail} className="cover-book" />
+              <h3 className="title-book">{book.title}</h3>
+              <p className="release-book">{book.publishedDate}</p>
+              <h6 className="preview-book">{book.description}</h6>
+              <h6 className="categories-book">Categories : {book.categories}</h6>
+              <h6 className="author-book">Author : {book.authors}</h6>
             </div>
           </div>
         </div>
