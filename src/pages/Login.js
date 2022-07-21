3ff082d4c2d308b945002a6c2530bd27241873a0
abrnsyh <abriansyah1099@gmail.com>
2022-07-22 01:00:55 +0700

    Fully functional login and register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,126 +3,146 @@ import React, { useState, useEffect } from "react";
 import jwt_decode from "jwt-decode";
 import { useSelector, useDispatch } from "react-redux";
 import { addUser, fetchUsers, addUsersApi } from "../components/User/UserSlice";
-import { Link, Navigate, useNavigate } from "react-router-dom";
-import { Alert, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { Alert } from "react-bootstrap";
 import { HiOutlineMailOpen } from "react-icons/hi";
 import { IoIosEye } from "react-icons/io";
 
 const Login = (props) => {
-  const [loginData, setLoginData] = useState({});
-  const [isLogin, setIsLogin] = useState(false);
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.user);
-  const navigate = useNavigate();
+	const [loginData, setLoginData] = useState({});
+	const [isLogin, setIsLogin] = useState(false);
+	const dispatch = useDispatch();
+	const user = useSelector((state) => state.user);
+	const navigate = useNavigate();
+	const [show, setShow] = useState(false);
+	const [loginCheck, setLoginCheck] = useState(null);
 
-  const [show, setShow] = useState(false);
+	useEffect(() => {
+		dispatch(fetchUsers());
+	}, []);
 
-  const handleInput = (event) => {
-    setLoginData({ ...loginData, [event.target.name]: event.target.value });
-  };
+	const handleInput = (event) => {
+		setLoginData({ ...loginData, [event.target.name]: event.target.value });
+	};
 
-  const handleLogIn = (event) => {
-    event.preventDefault();
-    const result = user.data.find((person) => person.email === loginData.email && person.password === loginData.password);
-    if (!result) {
-      setShow(true);
-    }
-  };
+	const handleLogIn = (event) => {
+		event.preventDefault();
+		let data = {
+			userBooks: [],
+			userData: [
+				{
+					email: loginData.email,
+					password: loginData.password,
+				},
+			],
+		};
+		let temp = user.data.find((users) => users.userData[0].email === data.userData[0].email);
+		console.log(temp?.userData[0]?.password, data.userData[0].email);
+		if (temp?.userData[0]?.email === data.userData[0].email && temp?.userData[0]?.password === data.userData[0].password) {
+			alert("login berhasil");
+			dispatch(addUser(temp));
+			setLoginCheck(true);
+			setTimeout(() => navigate("/dashboard"), 2000);
+		} else {
+			setShow(true);
+		}
+	};
 
-  // function for checking if the user is exist in the api
-  const checkUser = () => {
-    let temp = user?.data?.find((users) => users?.userData[0]?.email == loginData.userData[0].email);
-    console.log(temp, user);
-    return temp !== undefined ? (dispatch(addUser(temp)), navigate("/dashboard")) : (dispatch(addUsersApi(loginData)), dispatch(addUser(loginData), navigate("/dashboard")));
-  };
+	// function for checking if the user is exist in the api
+	const checkUser = () => {
+		let temp = user?.data?.find((users) => users?.userData[0]?.email == loginData.userData[0].email);
+		console.log(temp, user);
+		return temp !== undefined ? (dispatch(addUser(temp)), navigate("/dashboard")) : (dispatch(addUsersApi(loginData)), dispatch(addUser(loginData), navigate("/dashboard")));
+	};
 
-  // a handle call back function from google library
-  const handleCallbackResponse = (response) => {
-    // console.log('Encode JWT ID token: ' + response.credential)
-    const userObject = jwt_decode(response.credential);
-    let newUser = {
-      userBooks: [],
-      userData: [{ name: userObject.name, avatar: userObject.picture, email: userObject.email }],
-    };
-    setLoginData(newUser);
-    setIsLogin(true);
-  };
+	// a handle call back function from google library
+	const handleCallbackResponse = (response) => {
+		// console.log('Encode JWT ID token: ' + response.credential)
+		const userObject = jwt_decode(response.credential);
+		let newUser = {
+			userBooks: [],
+			userData: [{ name: userObject.name, avatar: userObject.picture, email: userObject.email }],
+		};
+		setLoginData(newUser);
+		setIsLogin(true);
+	};
 
-  if (isLogin) {
-    checkUser();
-    setIsLogin(false);
-  }
+	if (isLogin) {
+		checkUser();
+		setIsLogin(false);
+	}
 
-  // load library from google
-  useEffect(() => {
-    dispatch(fetchUsers());
-    /* global google */
-    google.accounts.id.initialize({
-      client_id: "310452483381-gb8cjmh5ti97985esf00r99bocp1kmke.apps.googleusercontent.com",
-      callback: handleCallbackResponse,
-    });
-    google.accounts.id.renderButton(document.getElementById("buttonDiv"), { theme: "outline", size: "large" });
-  }, []);
+	// load library from google
+	useEffect(() => {
+		/* global google */
+		google.accounts.id.initialize({
+			client_id: "310452483381-gb8cjmh5ti97985esf00r99bocp1kmke.apps.googleusercontent.com",
+			callback: handleCallbackResponse,
+		});
+		google.accounts.id.renderButton(document.getElementById("buttonDiv"), { theme: "outline", shape: "pill", width: "400px" });
+	}, []);
 
-  const sandi = () => {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  };
+	const sandi = () => {
+		let x = document.getElementById("password");
+		if (x.type === "password") {
+			x.type = "text";
+		} else {
+			x.type = "password";
+		}
+	};
 
-  return (
-    <div className="container">
-      <div className="container-fluid mx-auto">
-        <div className="row justify-content-center d-flex mx-auto">
-          <div className="shadow col-8 mt-1 py-3 px-3" style={{ backgroundColor: "white", borderRadius: "10px" }}>
-            <h1 className="justify-content-center d-flex mx-auto">Login</h1>
-            <p className="justify-content-center d-flex mx-auto">Hey, enter your details to sign in to your account</p>
-            {show && (
-              <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-                <Alert.Heading>sorry there was an error!</Alert.Heading>
-                <p>Please enter the correct email and password or register first</p>
-              </Alert>
-            )}
-            <div className="row mt-1">
-              <label for="exampleFormControlInput1" className="form-label">
-                Email
-              </label>
-              <div className="col-12">
-                <input type="email" className="form-control" id="emailView" name="email" placeholder="name@example.com" onChange={handleInput} />
-                <HiOutlineMailOpen style={{ position: "relative", top: "-55%", left: "90%" }} />
-              </div>
-            </div>
-            <div className="row mt-1">
-              <label for="exampleFormControlInput1" className="form-label">
-                Password
-              </label>
-              <div className="col-12">
-                <input type="password" className="form-control" id="password" name="password" value={loginData.password} onChange={handleInput} />
-                <IoIosEye onClick={sandi} style={{ position: "relative", top: "-50%", left: "90%" }} />
-              </div>
-            </div>
-            <br></br>
-            <button onClick={handleLogIn} style={{ backgroundColor: "#FFA000", border: "none", padding: "10px", borderRadius: "5px", color: "white" }}>
-              Sign In
-            </button>
+	return (
+		<div className="container">
+			<div className="container-fluid mx-auto">
+				<div className="row justify-content-center d-flex mx-auto">
+					<div className="shadow col-8 mt-1 py-3 px-3" style={{ backgroundColor: "white", borderRadius: "10px" }}>
+						<h1 className="justify-content-center d-flex mx-auto">Login</h1>
+						<p className="justify-content-center d-flex mx-auto">Hey, enter your details to sign in to your account</p>
+						{show && (
+							<Alert variant="danger" onClose={() => setShow(false)} dismissible>
+								<Alert.Heading>sorry there was an error!</Alert.Heading>
+								<p>Please enter the correct email and password or register first</p>
+							</Alert>
+						)}
+						<form onSubmit={handleLogIn}>
+							<div className="row mt-1">
+								<label htmlFor="exampleFormControlInput1" className="form-label">
+									Email
+								</label>
+								<div className="col-12">
+									<input type="email" className="form-control" id="emailView" name="email" placeholder="name@example.com" onChange={handleInput} required />
+									<HiOutlineMailOpen style={{ position: "relative", top: "-55%", left: "90%" }} />
+								</div>
+							</div>
+							<div className="row mt-1">
+								<label htmlFor="exampleFormControlInput1" className="form-label">
+									Password
+								</label>
+								<div className="col-12">
+									<input type="password" className="form-control" id="password" name="password" value={loginData.password} onChange={handleInput} required />
+									<IoIosEye onClick={sandi} style={{ position: "relative", top: "-50%", left: "90%" }} />
+								</div>
+							</div>
+							<br></br>
+							<button type="submit" style={{ backgroundColor: "#FFA000", border: "none", padding: "10px", borderRadius: "5px", color: "white" }}>
+								Sign In
+							</button>
+						</form>
 
-            <p className="mt-1">-Or Sign In With-</p>
-            <div classNameName="raw">
-              <div classNameName="col">
-                <div id="buttonDiv"></div>
-              </div>
-            </div>
-          </div>
-          {/* <div className="col-6 justify-content-end d-flex mx-auto my-auto">
+						<p className="mt-3 text-center">-Or Sign In With-</p>
+						<div className="raw">
+							<div className="col">
+								<div id="buttonDiv"></div>
+							</div>
+						</div>
+					</div>
+					{/* <div className="col-6 justify-content-end d-flex mx-auto my-auto">
             <img src={buku} width={500} height={500} /> */}
-          {/* </div> */}
-        </div>
-      </div>
-    </div>
-  );
+					{/* </div> */}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,100 +1,133 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { addUser, addUsersApi, fetchUsers } from "../components/User/UserSlice";
 
 const Signup = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.user);
-  const [userData, setUserData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-  });
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const user = useSelector((state) => state.user);
+	const [userData, setUserData] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+		password: "",
+	});
+	const [dataCheck, setDataCheck] = useState(null);
 
-  const handleChange = (event) => {
-    setUserData({ ...userData, [event.target.name]: event.target.value });
-  };
+	useEffect(() => {
+		dispatch(fetchUsers());
+	}, []);
 
-  const handleSumbit = (e) => {
-    // dispatch(fetchUsers())
-    e.preventDefault();
-    let data = {
-      userBooks: [],
-      userData: [
-        {
-          name: userData.firstName + " " + userData.lastName,
-          avatar: "https://bombyxplm.com/wp-content/uploads/2021/01/website-faculty-man-300x300.jpg",
-          email: userData.email,
-          password: userData.password,
-        },
-      ],
-    };
-    dispatch(addUsersApi(data));
-    dispatch(fetchUsers());
-  };
-  console.log(user);
+	const handleChange = (event) => {
+		setUserData({ ...userData, [event.target.name]: event.target.value });
+	};
 
-  const sandi = () => {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  };
+	const handleSumbit = (e) => {
+		e.preventDefault();
+		let data = {
+			userBooks: [],
+			userData: [
+				{
+					name: userData.firstName + " " + userData.lastName,
+					avatar: "https://bombyxplm.com/wp-content/uploads/2021/01/website-faculty-man-300x300.jpg",
+					email: userData.email,
+					password: userData.password,
+				},
+			],
+		};
+		let temp = user.data.find((users) => users.userData[0].email === data.userData[0].email);
+		console.log(temp, data.userData[0].email);
+		if (temp?.userData[0]?.email === data.userData[0].email) {
+			setDataCheck(true);
+		} else if (data?.userData[0]?.email.includes("@") && temp === undefined) {
+			dispatch(addUsersApi(data));
+			setDataCheck(false);
+			setTimeout(() => navigate("/log-in"), 3000);
+		} else {
+			setDataCheck(null);
+		}
+	};
+	console.log(dataCheck);
 
-  return (
-    <div className="container">
-      <div className="container mx-auto " style={{ backgroundColor: "#F7F1EA" }}>
-        <div className="row">
-          <div className="row justify-content-center d-flex mx-auto">
-            <div className="shadow col-8 mt-1 px-3 py-3" style={{ backgroundColor: "white", borderRadius: "10px" }}>
-              <p style={{ sizeText: "30px" }} className="my-3">
-                Create a new account
-              </p>
-              <div className="row">
-                <p>Already a member?</p>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <label htmlFor="exampleFormControlInput1" className="form-label">
-                    first name
-                  </label>
-                  <input type="text" className="form-control" id="first" name="firstName" value={userData.firstName} onChange={handleChange} />
-                </div>
-                <div className="col">
-                  <label htmlFor="exampleFormControlInput1" className="form-label">
-                    last name
-                  </label>
-                  <input type="text" className="form-control" id="last" name="lastName" value={userData.lastName} onChange={handleChange} />
-                </div>
-              </div>
-              <label htmlFor="exampleFormControlInput1" className="form-label pt-3">
-                Email
-              </label>
-              <div className="col-12">
-                <input type="email" className="form-control" id="emailView" name="email" placeholder="name@example.com" value={userData.email} onChange={handleChange} />
-              </div>
-              <label htmlFor="exampleFormControlInput1" className="form-label pt-3">
-                Password
-              </label>
-              <div className="col-12">
-                <input type="password" className="form-control" id="password" name="password" value={userData.password} onChange={handleChange} />
-              </div>
-              <br></br>
-              <button onClick={handleSumbit} style={{ backgroundColor: "#FFA000", border: "none", padding: "10px", color: "white", borderRadius: "5px" }}>
-                Create Account
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const sandi = () => {
+		var x = document.getElementById("password");
+		if (x.type === "password") {
+			x.type = "text";
+		} else {
+			x.type = "password";
+		}
+	};
+
+	return (
+		<div className="container">
+			<div className="container mx-auto " style={{ backgroundColor: "#F7F1EA" }}>
+				<div className="row">
+					<div className="row justify-content-center d-flex mx-auto">
+						<div className="shadow col-8 mt-1 px-3 py-3" style={{ backgroundColor: "white", borderRadius: "10px" }}>
+							<p style={{ sizeText: "30px" }} className="my-3">
+								Create a new account
+							</p>
+							<div className="row">
+								<div className="d-flex">
+									<p>Already a member?</p>
+									<Link to="/log-in">
+										<span className="mx-2">Login</span>
+									</Link>
+								</div>
+							</div>
+							<form onSubmit={handleSumbit}>
+								<div className="row">
+									<div className="col">
+										<label htmlFor="exampleFormControlInput1" className="form-label">
+											first name
+										</label>
+										<input type="text" className="form-control" id="first" name="firstName" value={userData.firstName} onChange={handleChange} required />
+									</div>
+									<div className="col">
+										<label htmlFor="exampleFormControlInput1" className="form-label">
+											last name
+										</label>
+										<input type="text" className="form-control" id="last" name="lastName" value={userData.lastName} onChange={handleChange} />
+									</div>
+								</div>
+								<label htmlFor="exampleFormControlInput1" className="form-label pt-3">
+									Email
+								</label>
+								<div className="col-12">
+									<input type="email" className="form-control" id="emailView" name="email" placeholder="name@example.com" value={userData.email} onChange={handleChange} required />
+								</div>
+								<label htmlFor="exampleFormControlInput1" className="form-label pt-3">
+									Password
+								</label>
+								<div className="col-12">
+									<input type="password" pattern=".{8,20}" className="form-control" id="password" name="password" value={userData.password} onChange={handleChange} required />
+								</div>
+								<div id="passwordHelpBlock" className="form-text">
+									Your password must be 8-20 characters long.
+								</div>
+								<br></br>
+								<button type="submit" style={{ backgroundColor: "#FFA000", border: "none", padding: "10px", color: "white", borderRadius: "5px" }}>
+									Create Account
+								</button>
+							</form>
+							{dataCheck === true ? (
+								<div className="alert alert-danger alert-dismissible fade show mt-3" role="alert">
+									Sorry but the email has been taken. Please log in to your account
+									<button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={() => setDataCheck(null)}></button>
+								</div>
+							) : dataCheck === false ? (
+								<div className="alert alert-success alert-dismissible fade show mt-3" role="alert">
+									Account successfully created.
+									<button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={() => setDataCheck(null)}></button>
+								</div>
+							) : null}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Signup;
